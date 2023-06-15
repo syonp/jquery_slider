fbe9dd9765b2a0aa43c91c6bd6906c52ddd179a1
syonp <131326888+syonp@users.noreply.github.com>
2023-06-15 12:32:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// slider option
+
+$(document).ready(function(){
+    $('.slider').bxSlider({
+       auto: true,
+       mode: 'fade',
+       speed: 3000,
+       pager: true,
+       captions: true
+    });
+ });
